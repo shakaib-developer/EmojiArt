5a4b9277a5825537cf3f2f585b8cb7159f7381d5
Shakaib Akhtar <shakaib.developer@gmail.com>
2019-09-11 17:44:23 +0500

    completed lecture 12

---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Shakaib Akhtar on 11/09/2019.
+//  Copyright © 2019 iParagons. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var label: UILabel!
+}
